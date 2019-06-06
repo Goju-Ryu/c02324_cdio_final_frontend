c1866b6b7a5b385dc18ef7e2e7b2d1ebba4981d6
Mads Møller Jensen <mads1606@gmail.com>
2019-06-06 23:31:26 +0200

    Initial commit

---
+++ frontend/android/app/src/main/java/dtu/swt/course02324/spring2019/group14/frontend/MainActivity.java
@@ -0,0 +1,13 @@
+package dtu.swt.course02324.spring2019.group14.frontend;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
