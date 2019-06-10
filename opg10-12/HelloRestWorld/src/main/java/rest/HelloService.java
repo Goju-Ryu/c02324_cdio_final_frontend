8ac310a4b015996adfb22751128498861891a306
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-06-10 15:04:31 +0200

    Added @GET method for helloworld!

--- opg10-12/HelloRestWorld/src/main/java/rest/HelloService.java
+++ opg10-12/HelloRestWorld/src/main/java/rest/HelloService.java
@@ -1,5 +1,23 @@
 package rest;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import com.google.gson.JsonObject;
+
+@Path("hello")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class HelloService {
+
+    @GET
+    public JsonObject helloWorld(){
+        System.out.println("Hello world! This was a GET request!");
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("msg", "Hello World!");
+        return jsonObject;
+    }
 
 }
