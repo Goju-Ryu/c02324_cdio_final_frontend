832c0f175c20de0148e9e02148d981655ea3f2b8
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-06-10 19:31:52 +0200

    Added items from backend development to test it on a platform that for some reason actually worked.

---
+++ opg10-12/HelloRestWorld/src/main/java/rest/FoodService.java
@@ -0,0 +1,49 @@
+package rest;
+
+import Technical_Services.ECategory;
+import Technical_Services.ELocation;
+import Technical_Services.FoodDTO;
+import com.google.gson.JsonObject;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("food")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class FoodService {
+    //This method should return a specified food
+    @GET
+    @Path("{id}")
+    public String getFood(@PathParam("id") int id){
+        Map<Integer, FoodDTO> foodDTOMap = new HashMap<>();
+
+
+        FoodDTO food1 = new FoodDTO();
+        food1.setID(1);
+        food1.setName("Smør");
+        food1.setExpDate(new Date(10/06/2019));
+        food1.setCategory(ECategory.Dairy);
+        food1.setLocation(ELocation.Fridge);
+        foodDTOMap.put(1, food1);
+
+        FoodDTO food = foodDTOMap.get(id);
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("id", food.getID());
+        jsonObject.addProperty("name", food.getName());
+        jsonObject.addProperty("expDate", DTS.dateToString(10, 06, 2019));
+        String foodCat = food.getCategory().toString();
+        String foodLoc = food.getLocation().toString();
+        jsonObject.addProperty("category", foodCat);
+        jsonObject.addProperty("location", foodLoc);
+        System.out.println(jsonObject.toString());
+        return jsonObject.toString();
+
+    }
+
+}
