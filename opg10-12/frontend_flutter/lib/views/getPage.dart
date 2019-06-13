7da89c274b644619c29edc6bbd9a3a6cdbc68ff0
Mads Møller Jensen <mads1606@gmail.com>
2019-06-13 21:34:36 +0200

    listSearch() now works. TODO implement this across the board

--- opg10-12/frontend_flutter/lib/views/getPage.dart
+++ opg10-12/frontend_flutter/lib/views/getPage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
 import 'package:frontend_flutter/rest.dart' as rest;
 import 'package:frontend_flutter/styles/TextStyles.dart';
-import 'package:provider/provider.dart';
 
 class GetPage extends StatefulWidget {
   @override
@@ -21,9 +23,12 @@ class _GetPageState extends State<GetPage> {
   void listSearch() async {
     print("ListSearch() - started");
     final foodListState = Provider.of<GetList>(context);
-    List<FoodLineDisplay> newList = List<FoodLineDisplay>();
+    foodListState.setIsLoading(true);
 
-    foodListState.setGetList(rest.getList("Pur"));
+    rest.getList("Pur").then((list) {
+      foodListState.setIsLoading(false);
+      foodListState.setGetList(list);
+    });
   }
 
   void idSearch(int id) {
@@ -31,56 +36,60 @@ class _GetPageState extends State<GetPage> {
     final foodListState = Provider.of<GetList>(context);
     List<FoodLineDisplay> newList = List<FoodLineDisplay>();
 
-    final future = rest.get("Pur", id);
-    print(future.toString());
+
   }
+
 
   @override
   Widget build(BuildContext context) {
     final foodListState = Provider.of<GetList>(context);
-    return new Center(
-        child: new Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        new Text(
-          "Get your food!",
-          style: MyHeadline,
-        ),
-        new Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-                width: 80, child: new Text("Check to search for all food: ")),
-            new Checkbox(value: _getList, onChanged: setGetList),
-            new Flexible(
-                //This is needed for the row to determine the size of textField. Without it an Error occurs and it isn't displayed.
-                flex: 1,
-                child: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), hintText: 'Enter id'),
-                  keyboardType: TextInputType.number,
-                  controller: textController,
-                )),
-            new RaisedButton(
-              onPressed: () {
-                if (_getList) {
-                  listSearch();
-                } else {
-                  idSearch(int.parse(textController.text));
-                }
-              }, //onPressed
-              child: new Text("Search"),
-              shape: new RoundedRectangleBorder(),
-              color: Colors.blue,
-            ),
-          ],
-        ),
-        new Expanded(child: new FoodList()),
-        //TODO: implement the food being displayed (create food view?)
-      ],
-    ));
+    return
+      new Center (
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              new Text("Get your food!", style: MyHeadline,),
+              new Row (
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(width: 80,
+                      child: new Text("Check to search for all food: ")),
+                  new Checkbox(value: _getList, onChanged: setGetList),
+                  new Flexible( //This is needed for the row to determine the size of textField. Without it an Error occurs and it isn't displayed.
+                      flex: 1,
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Enter id'
+                        ),
+                        keyboardType: TextInputType.number,
+                        controller: textController,
+                      )
+                  ),
+                  new RaisedButton(
+                    onPressed: () {
+                      if (_getList) {
+                        listSearch();
+                      } else {
+                        idSearch(int.parse(textController.text));
+                      }
+                    }, //onPressed
+                    child: new Text("Search"),
+                    shape: new RoundedRectangleBorder(),
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+              new Expanded(
+                  child: new FoodList()
+              ),
+              //TODO: implement the food being displayed (create food view?)
+
+            ],
+          )
+      );
   }
 
   @override
@@ -88,66 +97,54 @@ class _GetPageState extends State<GetPage> {
     textController.dispose();
     super.dispose();
   }
+
 }
 
 class FoodList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final foodList = Provider.of<GetList>(context);
-    return new ListView(
-      children: foodList.getFoodLineList(),
-    );
+    if (foodList.getIsLoading()) {
+      return new CircularProgressIndicator();
+    } else {
+      return new ListView(
+        children: foodList.getGetList(),
+      );
+    }
   }
+
 }
 
 class GetList with ChangeNotifier {
-  Future<List<rest.Food>> _list;
+  List<FoodLineDisplay> _list;
+  bool _isLoading;
 
-  GetList(this._list);
+  GetList(this._list) {
+    this._isLoading = false;
+  }
 
-  Future<List<rest.Food>> getGetList() {
+  List<FoodLineDisplay> getGetList() {
     return _list;
   }
 
-  void setGetList(Future<List<rest.Food>> list) {
-    _list = list;
+  void setGetList(List<rest.Food> list) {
+    List<FoodLineDisplay> foodLines = new List<FoodLineDisplay>();
+    list.forEach((f) {
+      foodLines.add(FoodLineDisplay(f));
+    });
+    _list = foodLines;
     notifyListeners();
   }
 
-  getFoodLineList() {
-    return FutureBuilder<List<rest.Food>>(
-      future: _list,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          print(snapshot.error.toString());
-          return Text(
-            snapshot.error.toString(),
-            style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.normal,
-                decoration: TextDecoration.none,
-                color: Colors.red),
-          );
-        } else if (snapshot.hasData) {
-          print("Success");
-          List<FoodLineDisplay> list;
-          for (int i = 0; i < snapshot.data.length; i++) {
-            list.add(new FoodLineDisplay(snapshot.data[i]));
-          }
-          return ListView.builder(
-              itemCount: list.length,
-              itemBuilder: (context, index) {
-                return list[index];
-              }); // a code fragment gotten from https://stackoverflow.com/questions/55011075/return-a-list-from-a-future-in-flutter
-        }
 
-        //by default, show a loading spinner
-        print("Loading");
-        //return CircularProgressIndicator();
-      },
-    );
+  void setIsLoading(bool isLoading) {
+    this._isLoading = isLoading;
   }
+
+  bool getIsLoading() {
+    return this._isLoading;
+  }
+
 }
 
 class FoodLineDisplay extends StatelessWidget {
