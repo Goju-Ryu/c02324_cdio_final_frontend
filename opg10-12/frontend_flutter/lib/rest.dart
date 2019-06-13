0792b4e343f3a3eb65afd1adf33484b8e97a34f8
Jens.Agersgaard <alex.norgaard@gmail.com>
2019-06-13 14:45:47 +0200

    All new http connections on each method in rest.dart

--- opg10-12/frontend_flutter/lib/rest.dart
+++ opg10-12/frontend_flutter/lib/rest.dart
@@ -8,14 +8,13 @@ class Food {
   final int id;
   final String name, date, foodCat, foodLoc, userName;
 
-  Food({
-      this.id,
+  Food(
+      {this.id,
       this.name,
       this.date,
       this.foodCat,
       this.foodLoc,
-      this.userName
-  });
+      this.userName});
 
   factory Food.fromJson(Map<String, dynamic> json) {
     return Food(
@@ -30,9 +29,8 @@ class Food {
 }
 
 Future<Food> get(String userName, int foodId) async {
-
   String url = _root + userName + '/get/' + foodId.toString();
-  Map<String, String> headers = {"Content-type" : "application/json"};
+  Map<String, String> headers = {"Content-type": "application/json"};
 
   final response = await http.get(url, headers: headers);
 
@@ -61,9 +59,9 @@ Future<Food> get(String userName, int foodId) async {
 Future<List<Food>> getList(String userName) async {
   print("Kommer den her bd?");
   String url = _root + userName + '/get';
-  Map<String, String> headers = {"Content-type" : "application/json"};
+  Map<String, String> headers = {"Content-type": "application/json"};
 
-  final response =  await http.get(url, headers: headers);
+  final response = await http.get(url, headers: headers);
 
   print("på den anden side.");
   print(response != null ? response.body : "Den er null");
@@ -91,24 +89,33 @@ Future<List<Food>> getList(String userName) async {
 }
 
 Future<String> sendPost(String userName, {Map body}) async {
+  String url = _root;
+  body = {"Content-type": "application/json"};
+
+  final response = await http.post(url, body: body);
+
+/*
   return http
       .post(_root, body: body)
       .then((http.Response response) {
     final int statusCode = response.statusCode;
-
-    if (statusCode == 201) {
-      //400
-      return "Food successfully created";
-    } else {
-      print('Error in food creation');
-      print("Error type: " + response.statusCode.toString());
-      print(response.body);
-      throw Exception('Failed to create food');
-    }
-  });
+*/
+  if (response.statusCode == 201) {
+    return "Food successfully created";
+  } else {
+    print('Error in food creation');
+    print("Error type: " + response.statusCode.toString());
+    print(response.body);
+    throw Exception('Failed to create food');
+  }
 }
 
-Future<String> deleteFood(int id, String userName) async {
+Future<String> deleteFood(int foodId, String userName) async {
+  String url = _root + userName + '/delete/' + foodId.toString();
+
+  final response = await http.delete(url);
+
+  /*
   return http
       .delete(_root +
           userName +
@@ -116,57 +123,60 @@ Future<String> deleteFood(int id, String userName) async {
           id.toString())
       .then((http.Response response) {
     final int statusCode = response.statusCode;
+*/
+  if (response.statusCode == 200) {
+    return "Food successfully deleted"; //Post.fromJson(json.decode(response.body));
 
-    if (statusCode == 200) {
-      //400
-      return "Food successfully deleted"; //Post.fromJson(json.decode(response.body));
-
-    } else {
-      print("Error in deletion");
-      print("Error type: " + response.statusCode.toString());
-      print(response.body);
-      throw Exception("Something went wrong");
-    }
-  });
+  } else {
+    print("Error in deletion");
+    print("Error type: " + response.statusCode.toString());
+    print(response.body);
+    throw Exception("Something went wrong");
+  }
 }
 
 Future<String> deleteAllFoods(String userName, String location) async {
-  return http
+  String url = _root + userName + '/delete/all/' + location;
+  final response = await http.delete(url);
+
+  /*return http
       .delete(_root +
           userName +
           '/delete/all/' +
           location)
       .then((http.Response response) {
     final int statusCode = response.statusCode;
-
-    if (statusCode == 200) {
-      return "All your foods have successfully been deleted. Feel hungry yet?"; //Post.fromJson(json.decode(response.body));
-    } else {
-      print("Error in deletion");
-      print("Error type: " + response.statusCode.toString());
-      print(response.body);
-      throw Exception("Something went wrong");
-    }
-  });
+*/
+  if (response.statusCode == 200) {
+    return "All your foods have successfully been deleted. Feel hungry yet?"; //Post.fromJson(json.decode(response.body));
+  } else {
+    print("Error in deletion");
+    print("Error type: " + response.statusCode.toString());
+    print(response.body);
+    throw Exception("Something went wrong");
+  }
 }
 
 Future<String> sendPut(
-    String userName, String foodId, Map<String, String> headers) {
+    String userName, int foodId, Map<String, String> headers) async {
+  String url = _root + userName + '/put/' + foodId.toString();
+  final response = await http.put(url, headers: headers);
+
+  /*
   return http
       .put(
           _root + userName + '/put/' + foodId,
           headers: headers)
       .then((http.Response response) {
     final int statusCode = response.statusCode;
-
-    if (statusCode == 200) {
-      //400
-      return "The food item has been updated"; //Food.PutFromJson(userName, id,json.decode(response.body));
-    } else {
-      print("Error when updating");
-      print("Error type: " + response.statusCode.toString());
-      print(response.body);
-      throw Exception("Something went wrong");
-    }
-  });
+*/
+  if (response.statusCode == 200) {
+    //400
+    return "The food item has been updated"; //Food.PutFromJson(userName, id,json.decode(response.body));
+  } else {
+    print("Error when updating");
+    print("Error type: " + response.statusCode.toString());
+    print(response.body);
+    throw Exception("Something went wrong");
+  }
 }
