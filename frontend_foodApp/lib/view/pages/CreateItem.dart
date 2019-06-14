e67cdbc2e2672ccaa282ffd9c27953482b1cc4ab
Gustav Vang Klinker <gulle139@hotmail.com>
2019-06-14 15:25:18 +0200

    Initial layout for the create item page;

---
+++ frontend_foodApp/lib/view/pages/CreateItem.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+//import 'package:flutter_web_test/flutter_web_test.dart';
+//import 'package:flutter_web_ui/ui.dart';
+
+class CreateItem extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Column(
+      children: <Widget>[
+        Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              TextFormField( decoration: InputDecoration(
+                  labelText: "Name Food")),
+              TextFormField( decoration: InputDecoration(
+                  labelText: "Place Food")),
+              TextFormField( decoration: InputDecoration(
+                  labelText: "Cat Food")),
+              TextFormField( decoration: InputDecoration(
+                  labelText: "Date Food")),
+              const SizedBox(height: 30),
+              RaisedButton(
+                onPressed: () {},
+                child: const Text(
+                    'Send',
+                    style: TextStyle(fontSize: 20)
+                ),
+              ),
+            ]
+        ),
+      ],
+    );
+  }
+}
