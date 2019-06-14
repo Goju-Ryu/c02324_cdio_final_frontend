d1db178611465a53cf1615a9dbcd6c285e9fdbcc
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-06-14 16:04:43 +0200

    Added stdColours

---
+++ frontend_foodApp/lib/view/page/stdColours.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+final primaryColour = Colors.indigo;
+final secondaryColour = Colors.blueAccent;
+final btnDisabledColour = Colors.blueGrey;
+final btnHighlightColour = Colors.lightBlueAccent;
+final btnSplashColour = Colors.white70;
+final textColour = Colors.white;
