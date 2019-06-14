74c6c84299e6c3a6c36fa72acb27e3ed7fb98ce7
Mads Møller Jensen <mads1606@gmail.com>
2019-06-14 08:11:32 +0200

    Started working on the Add/Update page

--- opg10-12/frontend_flutter/lib/views/updatePage.dart
+++ opg10-12/frontend_flutter/lib/views/updatePage.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:frontend_flutter/util/TextStyles.dart';
+import 'package:frontend_flutter/util/sharedStates.dart';
+import 'package:provider/provider.dart';
 
-import 'package:frontend_flutter/rest.dart' as rest;
+class UpdatePage extends StatefulWidget {
+  _UpdatePageState createState() => _UpdatePageState();
+}
 
-class UpdatePage extends StatelessWidget{
+class _UpdatePageState extends State<UpdatePage> {
+  bool addNew = true;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Text("Update Food");
+    final appState = Provider.of<AppState>(context);
+
+    return new Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        new Text(
+          "Add/Update",
+          style: MyHeadline,
+        ),
+        new Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                //TODO insert all the fields here
+                new Column(
+                  children: <Widget>[new Text("id"), TextField(
+                    //TODO insert textController here
+                  )],
+                )
+              ],
+            )
+          ],
+        )
+      ],
+    );
   }
 }
