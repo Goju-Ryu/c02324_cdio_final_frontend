e1b054b5cc36003681a182799512c55159b967da
Mads Møller Jensen <mads1606@gmail.com>
2019-06-19 10:04:00 +0200

    Added Category enum

--- frontend_foodApp/lib/util/enums.dart
+++ frontend_foodApp/lib/util/enums.dart
@@ -1,13 +1,45 @@
-enum ELocation { freezer, fridge, pantry }
+enum ELocation {
+  Freezer, Fridge, Pantry
+}
 
 String getLocationName(ELocation loc) {
   switch (loc) {
+    case ELocation.Freezer:
+      return "Freezer";
+    case ELocation.Fridge:
+      return "Fridge";
+    case ELocation.Pantry:
+      return "Pantry";
+    default:
+      throw new Exception("UnKnown Location");
+  }
+}
 
-    case ELocation.freezer:
-      return "freezer";
-    case ELocation.fridge:
-      return "fridge";
-    case ELocation.pantry:
-      return "pantry";
+
+enum ECategory {
+  Fish, Beef, Poultry, Pork, Vegetable, Fruit, Dairy, Bread, Other
+}
+
+String getCategoryName(ECategory cat) {
+  switch (cat) {
+
+    case ECategory.Fish:
+      return "Fish";
+    case ECategory.Beef:
+      return "Beef";
+    case ECategory.Poultry:
+      return "Poultry";
+    case ECategory.Pork:
+      return "Pork";
+    case ECategory.Vegetable:
+      return "Vegetables";
+    case ECategory.Fruit:
+      return "Fruit";
+    case ECategory.Dairy:
+      return "Daiary";
+    case ECategory.Bread:
+      return "Bread";
+    case ECategory.Other:
+      return "Other";
   }
 }
